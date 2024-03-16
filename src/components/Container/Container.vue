59b977655ed0b61cde7e25c2a92c32d533665dc6
victormedeiros1 <josevictordev@gmail.com>
2024-03-16 19:03:01 -0300

    feat: component Container

---
+++ src/components/Container/Container.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+    .container {
+        width: 100%;
+        max-width: 800px;
+        min-height: 100vh;
+        margin: 0 auto;
+        border-right: 1px solid $gray;
+        border-left: 1px solid $gray;
+        padding: $p-64;
+    }
+</style>
