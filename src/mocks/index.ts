a2498d1a2ac8ef336779d6a8b265ff55f805e4e9
victormedeiros1 <josevictordev@gmail.com>
2024-04-25 13:06:10 -0300

    chore: change players with teachers

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,194 +1,255 @@
 import { Customer } from "@/types/Customers";
 
 export const customers: Customer[] = [
-    {
-        id: 0,
-        name: 'Marquinhos',
-        birthday: '01/01/2001'
-    },
-    {
-        id: 1,
-        name: 'Everton Ribeiro',
-        birthday: '02/01/2003'
-    },
-    {
-        id: 2,
-        name: 'Bruno Henrique',
-        birthday: '03/01/1995'
-    },
-    {
-        id: 3,
-        name: 'José Victor',
-        birthday: '05/08/1975'
-    },
-    {
-        id: 4,
-        name: 'Arrascaeta',
-        birthday: '05/08/2000'
-    },
-    {
-        id: 5,
-        name: 'Gabriel Barbosa',
-        birthday: '05/08/2004'
-    },
-    {
-        id: 6,
-        name: 'Pedro',
-        birthday: '12/07/2004'
-    },
-    {
-        id: 7,
-        name: 'Leo Pereira',
-        birthday: '12/07/2004'
-    },
-    {
-        id: 8,
-        name: 'Fabricio Bruno',
-        birthday: '12/07/2004'
-    },
-    {
-        id: 9,
-        name: 'Haaland',
-        birthday: '12/07/2004'
-    },
-    {
-        id: 10,
-        name: 'Bellingham',
-        birthday: '12/07/2004'
-    },
-    {
-        id: 11,
-        name: 'João Gomes',
-        birthday: '01/01/1988'
-    },
-    {
-        id: 12,
-        name: 'Casemiro',
-        birthday: '24/03/1988'
-    },
-    {
-        id: 13,
-        name: 'Vinicius Junior',
-        birthday: '05/08/1999'
-    },
-    {
-        id: 14,
-        name: 'Cristiano Ronaldo',
-        birthday: '05/08/2003'
-    },
-    {
-        id: 15,
-        name: 'Tony Kroos',
-        birthday: '04/08/1999'
-    },
-    {
-        id: 15,
-        name: 'Militão',
-        birthday: '04/08/1999'
-    },
-    {
-        id: 16,
-        name: 'Sergio Ramos',
-        birthday: '04/08/1999'
-    },
-    {
-        id: 17,
-        name: 'Raúl',
-        birthday: '04/08/1999'
-    },
-    {
-        id: 18,
-        name: 'Di Stéfano',
-        birthday: '03/08/1999'
-    },
-    {
-        id: 19,
-        name: 'Ronaldinho Gáucho',
-        birthday: '03/08/1999'
-    },
-    {
-        id: 20,
-        name: 'Ronaldo',
-        birthday: '03/08/1999'
-    },
-    {
-        id: 21,
-        name: 'Romário',
-        birthday: '02/08/1999'
-    },
-    {
-        id: 22,
-        name: 'Leandro',
-        birthday: '02/08/1999'
-    },
-    {
-        id: 23,
-        name: 'Bruno Fernandes',
-        birthday: '01/08/1999'
-    },
-    {
-        id: 24,
-        name: 'Grealish',
-        birthday: '06/08/2003'
-    },
-    {
-        id: 25,
-        name: 'Firmino',
-        birthday: '06/08/2003'
-    },
-    {
-        id: 26,
-        name: 'Veiga',
-        birthday: '06/08/2003'
-    },
-    {
-        id: 27,
-        name: 'Rice',
-        birthday: '06/08/2003'
-    },
-    {
-        id: 28,
-        name: 'Raphinha',
-        birthday: '06/08/2003'
-    },
-    {
-        id: 29,
-        name: 'Rodrygo',
-        birthday: '06/08/2003'
-    },
-    {
-        id: 30,
-        name: 'Mbappé',
-        birthday: '07/08/2003'
-    },
-    {
-        id: 31,
-        name: 'Griezmann',
-        birthday: '07/08/2003'
-    },
-    {
-        id: 32,
-        name: 'Drogba',
-        birthday: '07/08/2003'
-    },
-    {
-        id: 33,
-        name: 'Beraldo',
-        birthday: '07/08/2003'
-    },
-    {
-        id: 34,
-        name: 'Lucas Paquetá',
-        birthday: '07/08/2003'
-    },
-    {
-        id: 35,
-        name: 'Romarinho',
-        birthday: '07/08/2003'
-    },
-    {
-        id: 34,
-        name: 'Hulk',
-        birthday: '07/08/2003'
-    },
-]
+  {
+    id: 0,
+    name: "Adrianne Paula Vieira De Andrade",
+    birthday: "04/12/1991",
+  },
+  {
+    id: 1,
+    name: "Flavius Da Luz E Gorgonio",
+    birthday: "26/11/1970",
+  },
+  {
+    id: 2,
+    name: "Humberto Rabelo",
+    birthday: "13/04/1967",
+  },
+  {
+    id: 3,
+    name: "Karliane Medeiros Ovidio Vale",
+    birthday: "30/12/1976",
+  },
+  {
+    id: 4,
+    name: "Almir Miranda Ferreira",
+    birthday: "28/10/1962",
+  },
+  {
+    id: 5,
+    name: "Francisco Marcio Barboza",
+    birthday: "28/12/1979",
+  },
+  {
+    id: 6,
+    name: "Joao Batista Borges Neto",
+    birthday: "06/06/1983",
+  },
+  {
+    id: 7,
+    name: "Luiz Paulo De Assis Barbosa",
+    birthday: "19/07/1982",
+  },
+  {
+    id: 8,
+    name: "Fabricio Vale De Azevedo Guerra",
+    birthday: "26/01/1979",
+  },
+  {
+    id: 9,
+    name: "Gilson Gomes Da Silva",
+    birthday: "24/05/1972",
+  },
+  {
+    id: 10,
+    name: "Joao Paulo De Souza Medeiros",
+    birthday: "13/10/1985",
+  },
+  {
+    id: 11,
+    name: "Taciano De Morais Silva",
+    birthday: "25/01/1980",
+  },
+
+  // {
+  //     id: 0,
+  //     name: 'Marquinhos',
+  //     birthday: '01/01/2001'
+  // },
+  // {
+  //     id: 1,
+  //     name: 'Everton Ribeiro',
+  //     birthday: '02/01/2003'
+  // },
+  // {
+  //     id: 2,
+  //     name: 'Bruno Henrique',
+  //     birthday: '03/01/1995'
+  // },
+  // {
+  //     id: 3,
+  //     name: 'José Victor',
+  //     birthday: '05/08/1975'
+  // },
+  // {
+  //     id: 4,
+  //     name: 'Arrascaeta',
+  //     birthday: '05/08/2000'
+  // },
+  // {
+  //     id: 5,
+  //     name: 'Gabriel Barbosa',
+  //     birthday: '05/08/2004'
+  // },
+  // {
+  //     id: 6,
+  //     name: 'Pedro',
+  //     birthday: '12/07/2004'
+  // },
+  // {
+  //     id: 7,
+  //     name: 'Leo Pereira',
+  //     birthday: '12/07/2004'
+  // },
+  // {
+  //     id: 8,
+  //     name: 'Fabricio Bruno',
+  //     birthday: '12/07/2004'
+  // },
+  // {
+  //     id: 9,
+  //     name: 'Haaland',
+  //     birthday: '12/07/2004'
+  // },
+  // {
+  //     id: 10,
+  //     name: 'Bellingham',
+  //     birthday: '12/07/2004'
+  // },
+  // {
+  //     id: 11,
+  //     name: 'João Gomes',
+  //     birthday: '01/01/1988'
+  // },
+  // {
+  //     id: 12,
+  //     name: 'Casemiro',
+  //     birthday: '24/03/1988'
+  // },
+  // {
+  //     id: 13,
+  //     name: 'Vinicius Junior',
+  //     birthday: '05/08/1999'
+  // },
+  // {
+  //     id: 14,
+  //     name: 'Cristiano Ronaldo',
+  //     birthday: '05/08/2003'
+  // },
+  // {
+  //     id: 15,
+  //     name: 'Tony Kroos',
+  //     birthday: '04/08/1999'
+  // },
+  // {
+  //     id: 15,
+  //     name: 'Militão',
+  //     birthday: '04/08/1999'
+  // },
+  // {
+  //     id: 16,
+  //     name: 'Sergio Ramos',
+  //     birthday: '04/08/1999'
+  // },
+  // {
+  //     id: 17,
+  //     name: 'Raúl',
+  //     birthday: '04/08/1999'
+  // },
+  // {
+  //     id: 18,
+  //     name: 'Di Stéfano',
+  //     birthday: '03/08/1999'
+  // },
+  // {
+  //     id: 19,
+  //     name: 'Ronaldinho Gáucho',
+  //     birthday: '03/08/1999'
+  // },
+  // {
+  //     id: 20,
+  //     name: 'Ronaldo',
+  //     birthday: '03/08/1999'
+  // },
+  // {
+  //     id: 21,
+  //     name: 'Romário',
+  //     birthday: '02/08/1999'
+  // },
+  // {
+  //     id: 22,
+  //     name: 'Leandro',
+  //     birthday: '02/08/1999'
+  // },
+  // {
+  //     id: 23,
+  //     name: 'Bruno Fernandes',
+  //     birthday: '01/08/1999'
+  // },
+  // {
+  //     id: 24,
+  //     name: 'Grealish',
+  //     birthday: '06/08/2003'
+  // },
+  // {
+  //     id: 25,
+  //     name: 'Firmino',
+  //     birthday: '06/08/2003'
+  // },
+  // {
+  //     id: 26,
+  //     name: 'Veiga',
+  //     birthday: '06/08/2003'
+  // },
+  // {
+  //     id: 27,
+  //     name: 'Rice',
+  //     birthday: '06/08/2003'
+  // },
+  // {
+  //     id: 28,
+  //     name: 'Raphinha',
+  //     birthday: '06/08/2003'
+  // },
+  // {
+  //     id: 29,
+  //     name: 'Rodrygo',
+  //     birthday: '06/08/2003'
+  // },
+  // {
+  //     id: 30,
+  //     name: 'Mbappé',
+  //     birthday: '07/08/2003'
+  // },
+  // {
+  //     id: 31,
+  //     name: 'Griezmann',
+  //     birthday: '07/08/2003'
+  // },
+  // {
+  //     id: 32,
+  //     name: 'Drogba',
+  //     birthday: '07/08/2003'
+  // },
+  // {
+  //     id: 33,
+  //     name: 'Beraldo',
+  //     birthday: '07/08/2003'
+  // },
+  // {
+  //     id: 34,
+  //     name: 'Lucas Paquetá',
+  //     birthday: '07/08/2003'
+  // },
+  // {
+  //     id: 35,
+  //     name: 'Romarinho',
+  //     birthday: '07/08/2003'
+  // },
+  // {
+  //     id: 34,
+  //     name: 'Hulk',
+  //     birthday: '07/08/2003'
+  // },
+];
